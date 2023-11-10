323464d59d4ec33d625d7dc83a7e4a072af7aad4
Randy Armstrong <randy@sparhawksoftware.com>
2023-11-09 22:24:30 -0800

    Update to OpenSSL 1.1.1w

--- fetch_openssl.bat
+++ fetch_openssl.bat
@@ -6,8 +6,8 @@ REM ****************************************************************************
 SETLOCAL
 
 set ROOT=%~dp0
-set SRCDIR=openssl-1.1.1t
-set GITTAG=OpenSSL_1_1_1t
+set SRCDIR=openssl-1.1.1w
+set GITTAG=OpenSSL_1_1_1w
 
 set GIT=C:\Program Files\Git\bin\git.exe
 IF NOT EXIST "%GIT%" SET GIT=C:\Program Files (x86)\Git\bin\git.exe
